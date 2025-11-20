b492deb32e24c540449394e24205cc3635283a7d
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2025-11-21 00:24:22 +0530

    Update user.routes.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { RegisterUser, loginUser, logoutUser} from '../controllers/user.controller.js'; 
-import { verifyJWT } from '../middlewares/auth.middlewares.js'; 
+import { verifyJWT } from './middlewares/auth.middlewares.js'; 
 
 const router= Router();
 
@@ -8,4 +8,5 @@ router.route("/register").post(RegisterUser);
 router.route("/login").post(loginUser);
 // secure route
 router.route("/logout").post(verifyJWT,logoutUser);
+
 export default router
